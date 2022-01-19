c721dbd4e187d5385f804bc128ca2e63c8b4608a
Daniel Pyon <pyondohyun@gmail.com>
2022-01-18 16:44:39 -0800

    parse instructions

--- gen_inst.py
+++ gen_inst.py
@@ -2,4 +2,12 @@ from peachpy.x86_64 import *
 import sys # for stdout buffer
 
 if __name__ == '__main__':
-    sys.stdout.buffer.write(bytes.fromhex('deadbeef'))
+    if len(sys.argv) <= 1:
+        print('Usage: python3 gen_inst.py [inst 1] ! [inst 2] ! [inst n]')
+    else:
+        instructions = list(map(lambda x: x.strip(), ' '.join(sys.argv[1:]).split('!')))
+
+        print(instructions)
+
+
+    # sys.stdout.buffer.write(bytes.fromhex('deadbeef'))
