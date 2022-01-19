a104a78d937d9b2f2d0beef098b4e56584d8c28d
Daniel Pyon <pyondohyun@gmail.com>
2022-01-18 16:30:37 -0800

    start project

---
+++ sim.py
@@ -0,0 +1,18 @@
+from unicorn import *
+from unicorn.x86_const import *
+
+X86_CODE64 = b'\x48\xc7\xc7\x37\x13\x00\x00\x48\xc7\xc0\x01\x00\x00\x00\x0f\x05'
+ADDRESS = 0x1000000
+
+try:
+    mu = Uc(UC_ARCH_X86, UC_MODE_64)
+    mu.mem_map(ADDRESS, 2 * 1024 * 1024)
+    mu.mem_write(ADDRESS, X86_CODE64)
+
+    mu.emu_start(ADDRESS, ADDRESS + len(X86_CODE64))
+    print('Emulation done. Below is CPU context.')
+
+    print(mu.reg_read(UC_X86_REG_RDI))
+
+except UcError as e:
+    print('Error: %s' % e)
