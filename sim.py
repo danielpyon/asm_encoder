ae433da718ac516bdd566d5df538c6e55958471f
Daniel Pyon <pyondohyun@gmail.com>
2022-01-18 20:13:44 -0800

    this works

--- sim.py
+++ sim.py
@@ -6,8 +6,9 @@ X86_CODE64 = b'\x48\xc7\xc7\x37\x13\x00\x00\x48\xc7\xc0\x01\x00\x00\x00\x0f\x05'
 ADDRESS = 0x1000000
 
 try:
-    if len(sys.argv) > 1:
-        X86_CODE64 = sys.argv[1]
+    instructions = sys.stdin.buffer.read()
+    if instructions is not None:
+        X86_CODE64 = instructions
 
     mu = Uc(UC_ARCH_X86, UC_MODE_64)
     mu.mem_map(ADDRESS, 2 * 1024 * 1024)
