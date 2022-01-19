59f4da9b1f55e1b0456fdf8c5e1b96d3f8f6c67b
Daniel Pyon <pyondohyun@gmail.com>
2022-01-18 17:12:15 -0800

    sim

--- sim.py
+++ sim.py
@@ -1,10 +1,14 @@
 from unicorn import *
 from unicorn.x86_const import *
+import sys
 
 X86_CODE64 = b'\x48\xc7\xc7\x37\x13\x00\x00\x48\xc7\xc0\x01\x00\x00\x00\x0f\x05'
 ADDRESS = 0x1000000
 
 try:
+    if len(sys.argv) > 1:
+        X86_CODE64 = sys.argv[1]
+
     mu = Uc(UC_ARCH_X86, UC_MODE_64)
     mu.mem_map(ADDRESS, 2 * 1024 * 1024)
     mu.mem_write(ADDRESS, X86_CODE64)
