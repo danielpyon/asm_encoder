bc0999914f933d90f24d4817a6813f0b7a1285c4
Daniel Pyon <pyondohyun@gmail.com>
2022-01-18 20:16:34 -0800

    error checking

--- gen_inst.sh
+++ gen_inst.sh
@@ -1,5 +1,10 @@
 # $1: name of assembly file
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Please supply the name of the assembly file'
+    exit 0
+fi
+
 gcc -nostdlib -static $1
 objcopy -O binary -j .text a.out out
 od -An -t x1 out | python3 raw.py
