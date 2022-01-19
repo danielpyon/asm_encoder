0135570045e27b9aa4972265308518c6096807fc
Daniel Pyon <pyondohyun@gmail.com>
2022-01-18 20:10:06 -0800

    raw done

--- raw.py
+++ raw.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     with fileinput.input() as f_input:
         instructions = sanitize(' '.join([line for line in f_input]))
         
-        for i in range(0, len(instructions), 2):
-            inst = instructions[i:i+2]
-            print(inst)
+        encoded = bytes.fromhex(instructions)
+        sys.stdout.buffer.write(encoded)
+
